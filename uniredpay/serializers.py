573e48aa869ad3f6aec0dd5f990e19186f15feba
Axrorbek1997 <axrorbekbebitovich97@gmail.com>
2021-12-11 16:02:53 +0500

    online wallet serializer commit

--- uniredpay/serializers.py
+++ uniredpay/serializers.py
@@ -14,3 +14,28 @@ class CourseOrderSerializers(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = '__all__'
+
+
+class UserSMSSerializers(serializers.Serializer):
+    card_number = serializers.RegexField(regex=r'9860\d{12}|8600\d{12}')
+    expire = serializers.CharField()
+    amount = serializers.IntegerField(default=0)
+    is_save = serializers.BooleanField(default=False)
+
+
+class PayForBalanceSMSSerializer(serializers.Serializer):
+    sms_code = serializers.CharField()
+
+
+class PayForCourseWithoutSMSSerializer(serializers.Serializer):
+    course_id = serializers.CharField()
+
+
+class PayForCourseSMSSerializer(serializers.Serializer):
+    sms_code = serializers.CharField()
+    course_id = serializers.CharField()
+
+
+class SpeakerCardSerializers(serializers.Serializer):
+    card_number = serializers.RegexField(regex=r'9860\d{12}|8600\d{12}')
+    expire = serializers.CharField()
